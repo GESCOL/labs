20d270d361b24e5bc05205d12b183a4c2cdd35ab
judgonzalezmu <judgonzalezmu@unal.edu.co>
2020-10-07 11:40:29 -0500

    Clases entidad

---
+++ src/Entidad/Usuario.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+/**
+ *
+ * @author juand
+ */
+public class Usuario {
+    
+    private String nombre;
+    private String password;
+    private int id;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public Usuario(){
+    }
+}
