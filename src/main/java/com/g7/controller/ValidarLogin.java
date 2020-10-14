f8e3aec6fac2a922cd90af83805c2f82594911a8
ssolanom <ssolanom@unal.edu.co>
2020-10-13 21:15:17 -0500

    fixed CORS

--- src/main/java/com/g7/controller/ValidarLogin.java
+++ src/main/java/com/g7/controller/ValidarLogin.java
@@ -3,6 +3,7 @@ package com.g7.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.g7.entidad.Usuario;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin (origins = "http://127.0.0.1:8887")
 public class ValidarLogin {
 	
 	private UsuarioDAO dao = new UsuarioDAO();
